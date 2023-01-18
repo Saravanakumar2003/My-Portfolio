6db0839e79bd45045396b5f673006ba6d4c8f6b8
Saravanakumar R <100985347+Saravanakumar2003@users.noreply.github.com>
2023-01-18 13:39:44 +0530

    s

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
 const scrollHeader = () =>{
     const header = document.getElementById('header')
     // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
-    this.scrollY >= 50 ? header.classList.add('scroll-header') 
+    this.scrollY >= 100 ? header.classList.add('scroll-header') 
                        : header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
