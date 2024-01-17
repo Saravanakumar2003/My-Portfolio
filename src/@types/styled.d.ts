46b2ba9fedd2e188a14ab3a3ea40eec14d0ea25e
Saravanakumar R <100985347+Saravanakumar2003@users.noreply.github.com>
2024-01-17 07:14:20 +0530

    Deleting Unwanted

--- src/@types/styled.d.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-import { defaultTheme } from "../styles/themes/default";
-
-type ThemeType = typeof defaultTheme;
-
-declare module "styled-components" {
-  export interface DefaultTheme extends ThemeType {}
-}
